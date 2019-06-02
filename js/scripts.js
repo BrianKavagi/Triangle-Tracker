6c15114ecac78e86f36d2bc05684d433b15c2129
BrianKavagi <briankavagi06@gmail.com>
2019-06-02 22:32:24 +0300

    Javascript2

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
-
-var sideX = parseInt(prompt("Enter Side X"));
-var sideY = parseInt(prompt("Enter Side Y"));
-var sideZ = parseInt(prompt("Enter Side Z"));
-if (sideX===sideY===sideZ);
-alert("EQUILATERAL TRIANGLE");
+function formvalidation(){
+var sideX=parseInt(document.getElementById('sideX').value);
+var sideY=parseInt(document.getElementById('sideY').value);
+var sideZ=parseInt(document.getElementById('sideZ').value);
+if (sideX==sideY && sideY==sideZ){
+alert("Triangle is Equilateral");
+}
+else if (sideX==sideY || sideX==sideZ || sideY==sideZ){
+alert("Triangle is isoceles");
+}else if(sideX <=(sideY + sideZ) || sideY <= (sideZ + sideX) || sideZ <= (sideX + sideY)){
+alert("Not A Triangle");
+}else{
+alert("scalene");
+}
+}
+return(formvalidation());
+function reset(){
+   var sideX=parseInt(document.getElementById('sideZ').value);
+   var sideY=parseInt(document.getElementById('sideZ').value);
+   var sideZ=parseInt(document.getElementById('sideZ').value);
+}
